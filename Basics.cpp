1edfdcf2bd5c53b87888e0d3109ef2d8fc5aa05a
spsvarshini <saivarshini2706@gmail.com>
2025-06-03 06:27:18 +0530

    Create Basics.cpp

---
+++ Basics.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //data types and veriables 
+    //operatores 
+    //coditional statements 
+    //control (loops)
+    //number programs
+    //arrays
+    //strings 
+    //functions
+    //problems 
+
+    //1,data types and veriables 
+    //int , float, bool , char 
+    int a = 10; // 4 bytes -> each byte will 8 bits
+    //total 4 * 8 = 32 bits 0 to 2^31-1 
+    //a to z , A to Z , 1 2 3 , A1 A2,_ 
+    //Code readibility --> 
+    int num1 = 10;
+    long long num2 = 1e18
+    float x = 2.5;
+    double d = 2.978234632 
+    bool state = true;
+    char c = 'A';
+
+    //OPeratores 
+    //1,Arthmatic (+, - , * , / , %)
+    int a = 10, b = 20;
+    // a + b , a - b = -10 , a * b , a /b , a % b
+
+    //2,Relational / comperasion, < , >= , <= , == , !=)
+    //a > b -> false , a < b -> true , a!=b true 
+    // a == b -> false, a >= b (a > b || a == b) false
+    //a <= b (a < b || a == b) true 
+
+    //logical (and (&&) , or (||))
+    //year = 4th , brach = (cse , it , Aiml, Ds)
+    //backlogs = 0, cgp = 7.0
+
+
+    if(year == 4 && (branch == cse || branch == it || 
+        branch == Aiml || branch == Ds) && backlogs ==0 && Cgp >= 7.0 )
+
+    //cod 1 && cod 2 && cod 3 ..... cod 100
+
+    //cod 1 || cod 2 || cod 3 ...... cod 100
+
+
+    //bit wise (& , | , ^ , ~ , >> , <<)
+    int A = 10;
+    int B = 2 ;
+    A & B 
+    ~A --> 1 0 1 0 -> 0 1 0 1
+
+    //0 1 0 1 0
+    //0 0 0 1 0
+    //-----------
+    //0 0 0 1 0
+
+
+    //conditional (if, else if , else)
+    
+
+
+
+}
